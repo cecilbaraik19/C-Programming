21a59fe3b8461add73c571230c0f980bffab54d0
Cecil Git <cecilbaraik19@gmail.com>
2025-09-16 12:46:46 +0530

    Sum of

---
+++ Sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter the value of a\n");
+    scanf("%d", &a);
+    printf("Enter the value of b\n");
+    scanf("%d", &b);
+    printf("Value of a :%d\n", a);
+    printf("Value of a b:%d\n", b);
+    c = a + b;
+    printf("Sum of two no. :%d\n", c);
+    return 0;
+}
