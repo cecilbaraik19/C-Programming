4e8381e2f9439db8b06f3ae0a29faf7ddf9d1974
Cecil Git <cecilbaraik19@gmail.com>
2025-09-16 10:53:29 +0530

    details added

---
+++ Details1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char grade;
+    int roll;
+    float per;
+    double fee;
+    grade ='A';
+    roll = 006;
+    per = 89.98;
+    fee = 87880.89;
+    printf("Grade           :%c\n",grade);
+    printf("Roll            :%d\n",roll);
+    printf("Percentage      :%f\n",per);
+    printf("Fee             :%lf\n",fee);
+    return 0;
+}
