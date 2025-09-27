7fc67eba2cf5f512ea97beeeaebded925b367cf1
Cecil Git <cecilbaraik19@gmail.com>
2025-09-27 23:01:23 +0530

    basic Matrix Program

---
+++ 131_Matrix.c
@@ -0,0 +1,16 @@
+// Matrix simple program
+#include <stdio.h>
+int main()
+{
+    int i, j, arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    printf("Matrix Element\n");
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
